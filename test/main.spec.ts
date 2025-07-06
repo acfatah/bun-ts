93f798ea1cdfc3f9e0c920b7c7842fd5802920aa
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-06 11:27:26 +0800

    refactor: rename main entrypoint to index.ts

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -4,7 +4,7 @@ describe('main', () => {
   it('should print hello world', async () => {
     const consoleSpy = spyOn(console, 'log')
 
-    await import('../src/main.ts')
+    await import('../src/index.ts')
     expect(consoleSpy).toHaveBeenCalledWith('Hello, world!')
 
     consoleSpy.mockRestore()
